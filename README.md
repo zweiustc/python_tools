789d5dbb00e9d9c281a8c928515cccc7e33d1d32
zhangwei29 <zhangwwei29@baidu.com>
2015-01-27 10:33:43 +0800

    add a python tool find qemu-info execute time from logs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python_tools
 some tools in python
+Reserve some tools written in python to improve work efficiency.
---
+++ qemu_info.py
@@ -0,0 +1,32 @@
+import os
+import time
+import datetime
+
+#filename = raw_input("Enter your file name: ")
+filename = "a.log"
+
+_DEFAULT_LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+qemu_img="env LC_ALL=C LANG=C qemu-img info"
+qemu_result="Result was "
+
+def get_time(line):
+    str1 = line.split('.')
+    time=str1[0]
+    time=datetime.datetime.strptime(time, _DEFAULT_LOG_DATE_FORMAT)
+    return time
+
+file = open(filename)
+start = None
+standard = datetime.timedelta(seconds=40)
+for line in file:
+    if qemu_img in line:
+        start = get_time(line)
+    if qemu_result in line:
+        end = get_time(line)
+        if start:
+            delta = end - start
+            print delta
+        if delta > standard:
+            print "delta %s: line %s " % (delta, line)
+print "finished"
+file.close()
